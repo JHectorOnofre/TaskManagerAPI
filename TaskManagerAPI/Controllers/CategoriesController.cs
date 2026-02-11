58dbcb7123d5539082093c9505efa79febe738a2
Hector Onofre <jose.onofre@kratosconsultores.com.mx>
2026-02-10 18:34:29 -0600

    commit parte de la impolementación del catalogo_categorias (excel) en la parte del front

--- TaskManagerAPI/Controllers/CategoriesController.cs
+++ TaskManagerAPI/Controllers/CategoriesController.cs
@@ -10,7 +10,7 @@ using TaskManagerAPI.Models;
 
 namespace TaskManagerAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] // <- significa que la base es api/categories
     [ApiController]
     public class CategoriesController : ControllerBase
     {
@@ -57,93 +57,12 @@ namespace TaskManagerAPI.Controllers
             if (file == null || file.Length == 0)
                 return BadRequest("No se recibió ningún archivo o está vacío.");
 
-            // Llamamos al servicio y él se encarga de todo el "trabajo sucio"
+            // Llamamos al servicio y él se encarga de todo el trabajo con la lógica
             var count = await _categoryService.ImportCategoriesFromExcelAsync(file);
 
             return Ok(new { Message = $"Se importaron {count} categorías nuevas." });
         }
 
-        //    if (file == null || file.Length == 0)
-        //        return BadRequest("No se recibió ningún archivo o está vacío.");
-
-        //    var categories = new List<Category>();
-
-        //    using (var stream = new MemoryStream()) // Crear archivo virtual en la RAM
-        //    {
-        //        await file.CopyToAsync(stream);
-        //        stream.Position = 0; // Nos aseguramos de ir al inicio del archivo
-
-        //        using (var workbook = new XLWorkbook(stream)) //XLM librería para abrir el archivo copiado en la memoria virtual y moverse a través
-        //        {
-        //            var worksheet = workbook.Worksheets.First(); // Tomamos la primera hoja
-        //            var rows = worksheet.RangeUsed().RowsUsed(); // conteo de cuántas casillas del archivo se están usando para iterar sobre ellas
-
-        //            bool isHeader = true; // para excluir la primera fila (ya que en este caso se usa como cabecera)
-
-        //            foreach (var row in rows) //recorrer las casillas en uso 
-        //            {
-        //                if (isHeader)
-        //                {
-        //                    // Saltar la fila de cabeceras
-        //                    isHeader = false;
-        //                    continue;
-        //                }
-        
-        //                // 3 líneas para extraer los datos de cada columna (de izq-derecha)
-        //                var name = row.Cell(1).GetString();      // Columna A
-        //                var code = row.Cell(2).GetString();      // Columna B
-        //                var isActiveCell = row.Cell(3).GetString(); // Columna C
-
-        //                bool isActive = true;
-        //                if (!string.IsNullOrWhiteSpace(isActiveCell))
-        //                {
-        //                    // TRUE/FALSE, 1/0, Sí/No... aquí se puede refinar
-        //                    bool.TryParse(isActiveCell, out isActive);
-        //                }
-
-        //                if (string.IsNullOrWhiteSpace(name))
-        //                {
-        //                    // Se puede decidir saltar o romper
-        //                    continue;
-        //                }
-
-        //                var category = new Category //lista vacía de la entidad categorías
-        //                {
-        //                    Name = name.Trim(),
-        //                    Code = code?.Trim(),
-        //                    IsActive = isActive
-        //                };
-
-        //                categories.Add(category);
-        //            }
-        //        }
-        //    }
-
-        //    // Validaciones 
-        //    // Opcional: filtrar duplicados por Name en la misma importación
-        //    categories = categories
-        //        .GroupBy(c => c.Name.ToLower())
-        //        .Select(g => g.First())
-        //        .ToList();
-
-        //    // Opcional: evitar insertar categorías que ya existan en la BD
-        //    var existingNames = _context.Categories
-        //        .Select(c => c.Name.ToLower())
-        //        .ToHashSet(); // Para rendimiento: Es un símil de trabajar en un Array con un índice (Trabaja de forma instantánea sin recorrer todo buscando)
-
-        //    var newCategories = categories
-        //        .Where(c => !existingNames.Contains(c.Name.ToLower()))
-        //        .ToList();
-
-        //    // Guardar en base de datos
-        //    _context.Categories.AddRange(newCategories);
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok(new
-        //    {
-        //        Message = $"Se importaron {categories.Count} categorías."
-        //    });
-        //}
     }
 
 }
